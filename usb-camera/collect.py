5d21ccb78e79b75d2985bb655954193de8d9881d
anarghya-das <anarghya@buffalo.edu>
2024-11-30 13:27:07 -0500

    updated with multiprocessing, which is not working currently

--- usb-camera/collect.py
+++ usb-camera/collect.py
@@ -1,14 +1,15 @@
 import cv2
 import signal
 import sys
-import threading
-from datetime import datetime
 import platform
+from multiprocessing import Process, Event
 from pylsl import StreamInfo, StreamOutlet
+from datetime import datetime
 
 
 class VideoRecorder:
-    def __init__(self, cam_id=0, output_path='output.avi', display_video=False, enable_lsl=False):
+    def __init__(self, cam_id=0, output_path='output.avi', default_fps=30, display_video=False, enable_lsl=False):
+        print("VideoRecorder start.")
         self.cam_id = cam_id
         self.output_path = output_path
         self.display_video = display_video
@@ -16,7 +17,9 @@ class VideoRecorder:
         self.cap = None
         self.out = None
         self.video_outlet = None
-        self.stop_event = threading.Event()
+        self.default_fps = default_fps
+        self.stop_event = Event()
+        print("VideoRecorder initialized successfully.")
 
     def signal_handler(self, sig, frame):
         print('Termination signal received. Releasing resources...')
@@ -34,18 +37,13 @@ class VideoRecorder:
         cv2.destroyAllWindows()
 
     def record_video(self):
-        backend = None
-        if platform.system() == 'Windows':
-            backend = cv2.CAP_DSHOW
+        backend = cv2.CAP_DSHOW if platform.system() == 'Windows' else None
         self.cap = cv2.VideoCapture(self.cam_id, backend)
 
-        fps = self.cap.get(cv2.CAP_PROP_FPS)
-        if fps == 0:
-            fps = 30
-
+        fps = self.cap.get(cv2.CAP_PROP_FPS) or self.default_fps
         if self.enable_lsl:
-            info = StreamInfo('VideoStream', 'Video', 1,
-                              fps, 'float32', 'videouid34234')
+            info = StreamInfo('VideoStream', 'Video', 1, fps,
+                              'float32', 'videouid34234')
             self.video_outlet = StreamOutlet(info)
 
         fourcc = cv2.VideoWriter_fourcc(*'XVID')
@@ -55,48 +53,52 @@ class VideoRecorder:
             self.output_path, fourcc, fps, (frame_width, frame_height))
 
         frame_number = 0
-        while self.cap.isOpened() and not self.stop_event.is_set():
-            ret, frame = self.cap.read()
-            if ret:
-                now = datetime.now()
-                timestamp = now.strftime("%Y-%m-%d %H:%M:%S.%f")
-                if self.video_outlet:
-                    self.video_outlet.push_sample([frame_number])
+        try:
+            while self.cap.isOpened() and not self.stop_event.is_set():
+                ret, frame = self.cap.read()
+                if ret:
+                    now = datetime.now()
+                    timestamp = now.strftime("%Y-%m-%d %H:%M:%S.%f")
+                    if self.video_outlet:
+                        self.video_outlet.push_sample([frame_number])
 
-                colour = (0, 255, 0)
-                origin = (10, 30)
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                scale = 1
-                thickness = 2
+                    colour = (0, 255, 0)
+                    origin = (10, 30)
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    scale = 1
+                    thickness = 2
 
-                cv2.putText(frame, timestamp, origin,
-                            font, scale, colour, thickness)
-                self.out.write(frame)
-                frame_number += 1
-                if self.display_video:
-                    cv2.imshow('frame', frame)
+                    cv2.putText(frame, timestamp, origin,
+                                font, scale, colour, thickness)
+                    self.out.write(frame)
+                    frame_number += 1
+                    if self.display_video:
+                        cv2.imshow('frame', frame)
 
-                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
+                else:
                     break
-            else:
-                break
-
-        self.release_resources()
+        finally:
+            self.release_resources()
 
 
 if __name__ == "__main__":
     recorder = VideoRecorder(
-        cam_id=1, output_path='output.avi', display_video=False, enable_lsl=True)
+        cam_id=1, output_path='video.avi', display_video=False, enable_lsl=True)
     signal.signal(signal.SIGINT, recorder.signal_handler)
     signal.signal(signal.SIGTERM, recorder.signal_handler)
 
-    # Create and start a thread to run the record_video method
-    recording_thread = threading.Thread(target=recorder.record_video)
-    recording_thread.start()
+    # Create and start a process to run the record_video method
+    recording_process = Process(target=recorder.record_video)
+    recording_process.start()
 
     # Example of stopping the recorder after 10 seconds
     try:
-        recording_thread.join(timeout=100)
+        import time
+        time.sleep(5)
+        recorder.stop()
+        # recording_process.join(timeout=10)
     except KeyboardInterrupt:
         recorder.stop()
-        recording_thread.join()
+        recording_process.join()
